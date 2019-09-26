76f036928ebf429c572719c801d33e99239ab9ac
lindsaymorales <41713266+lindsaymorales@users.noreply.github.com>
2019-09-25 20:10:27 -0600

    Update create tablespaces
    Creation of tablespace for schema PCADMIN

--- scripts_proyecto/crearTablespace.sql
+++ scripts_proyecto/crearTablespace.sql
@@ -1,4 +1,4 @@
---Creacion de tablespace
+--Creación de tablespace PC
 
 CREATE TABLESPACE PC_Data
        DATAFILE 'C:\app\HansF\oradata\dbprueba\pc_data01.dbf'
@@ -10,6 +10,24 @@ CREATE TABLESPACE PC_Data
 
 CREATE TABLESPACE PC_Ind
        DATAFILE 'C:\app\HansF\oradata\dbprueba\pc_ind01.dbf'
+       SIZE 10M
+       REUSE
+       AUTOEXTEND ON
+       NEXT 512k
+       MAXSIZE 200M;
+       
+--Creacion de tablespace PCADMIN
+
+CREATE TABLESPACE PCADMIN_Data
+       DATAFILE 'C:\app\lin\oradata\orcl\pcadmin_data01.dbf'
+       SIZE 10M
+       REUSE
+       AUTOEXTEND ON
+       NEXT 512k
+       MAXSIZE 200M;
+
+CREATE TABLESPACE PCADMIN_Ind
+       DATAFILE 'C:\app\lin\oradata\orcl\pcadmin_ind01.dbf'
        SIZE 10M
        REUSE
        AUTOEXTEND ON
