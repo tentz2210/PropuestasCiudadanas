b18527803e194a39a65c3aa97738cfa2c8affec1
lindsaymorales <41713266+lindsaymorales@users.noreply.github.com>
2019-09-26 11:15:43 -0600

    Update and rename TriggersAuditoría.sql to triggers.sql

--- scripts_proyecto/triggers.sql
+++ scripts_proyecto/triggers.sql
@@ -355,6 +355,15 @@ For each row
     :new.last_modify_date:= SYSDATE;
     :new.last_modified_by:= USER;
   END beforeUpdatePassword_Change;   
+  
+CREATE OR REPLACE TRIGGER PC.beforeUpdatePassword
+BEFORE UPDATE OF user_password
+ON person_user
+FOR EACH ROW
+  BEGIN 
+    INSERT INTO PCADMIN.password_change(change_number,id_user,date_of_change,old_password,new_password)
+    VALUES(PCADMIN.s_password_change_number.NEXTVAL,:new.id_user,SYSDATE,:old.user_password,:new.user_password);
+  END beforeUpdatePassword;
 
 
   
