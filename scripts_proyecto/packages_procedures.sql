c92228af94f0dfb0508fa5ef1dfffc9494f22008
mapachass16 <42397265+mapachass16@users.noreply.github.com>
2019-09-26 20:16:31 -0600

    Update packages
    Creation of pending packages

--- scripts_proyecto/packages_procedures.sql
+++ scripts_proyecto/packages_procedures.sql
@@ -183,3 +183,209 @@ CREATE OR REPLACE PACKAGE BODY pkg_person AS
     ------PROCEDURE DELETE------
     
 END pkg_person;
+
+----------PACKAGE EMAIL----------
+CREATE OR REPLACE PACKAGE pkg_email IS
+    PROCEDURE insertEmail(pMail VARCHAR2, pIdNumber NUMBER);
+END pkg_email;
+
+CREATE OR REPLACE PACKAGE BODY pkg_email AS
+    ------PROCEDURE INSERT------
+    PROCEDURE insertEmail(pMail VARCHAR2, pIdNumber NUMBER) IS
+    BEGIN
+        INSERT INTO pc.email(mail,id_number)
+        VALUES(pMail,pIdNumber);
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('Error inserting email');
+            DBMS_OUTPUT.PUT_LINE(SQLERRM);
+            DBMS_OUTPUT.PUT_LINE(SQLCODE);
+            ROLLBACK;
+    END;
+    ------PROCEDURE UPDATE------
+    
+    ------PROCEDURE DELETE------
+    
+END pkg_email;
+
+----------PACKAGE TELEPHONE----------
+CREATE OR REPLACE PACKAGE pkg_telephone IS
+    PROCEDURE insertTelephone(pTelephone NUMBER, pIdNumber NUMBER);
+END pkg_telephone;
+    
+CREATE OR REPLACE PACKAGE BODY pkg_telephone AS
+    ------PROCEDURE INSERT------
+    PROCEDURE insertTelephone(pTelephone NUMBER, pIdNumber NUMBER) IS
+    BEGIN
+        INSERT INTO pc.telephone(phone_number,id_number)
+        VALUES(pTelephone,pIdNumber);
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('Error inserting telephone');
+            DBMS_OUTPUT.PUT_LINE(SQLERRM);
+        	DBMS_OUTPUT.PUT_LINE(SQLCODE);
+            ROLLBACK;
+    END;
+    ------PROCEDURE UPDATE------
+    
+    ------PROCEDURE DELETE------
+    
+END pkg_telephone;
+    
+----------PACKAGE COMMENT----------
+CREATE OR REPLACE PACKAGE pkg_comment IS
+    PROCEDURE insertComment(pIdNumber NUMBER, pIdProposal NUMBER, pText VARCHAR2, pDate DATE);
+END pkg_comment;
+
+CREATE OR REPLACE PACKAGE BODY pkg_comment AS
+    ------PROCEDURE INSERT------
+    PROCEDURE insertComment(pIdNumber NUMBER, pIdProposal NUMBER, pText VARCHAR2, pDate DATE) IS
+    BEGIN
+        IF pDate IS NULL THEN
+            INSERT INTO pc.proposal_comment(code_comment,id_number,id_proposal,text)
+            VALUES(s_comment_code.NEXTVAL, pIdNumber, pIdProposal, pText);
+            COMMIT;
+        ELSE
+            INSERT INTO pc.proposal_comment(code_comment,id_number,id_proposal,text,date_time)
+            VALUES(s_comment_code.NEXTVAL, pIdNumber, pIdProposal, pText, TO_DATE(pDate, 'dd/mm/yyyy'));
+            COMMIT;
+        END IF;
+    EXCEPTION
+        WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('Error inserting comment');
+            DBMS_OUTPUT.PUT_LINE(SQLERRM);
+            DBMS_OUTPUT.PUT_LINE(SQLCODE);
+            ROLLBACK;
+    END;
+    ------PROCEDURE UPDATE------
+    
+    ------PROCEDURE DELETE------
+    
+END pkg_comment;
+    
+----------PACKAGE VOTE X PERSON----------
+CREATE OR REPLACE PACKAGE pkg_vote_x_person IS
+    PROCEDURE insertVote(pIdProposal NUMBER, pIdNumber NUMBER);
+END pkg_vote_x_person;
+
+CREATE OR REPLACE PACKAGE BODY pkg_vote_x_person AS
+    ------PROCEDURE INSERT------
+    PROCEDURE insertVote(pIdProposal NUMBER, pIdNumber NUMBER) IS
+    BEGIN
+        INSERT INTO pc.vote_x_person(id_proposal, id_number)
+        VALUES (pIdProposal, pIdNumber);
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('Error inserting vote');
+            DBMS_OUTPUT.PUT_LINE(SQLERRM);
+            DBMS_OUTPUT.PUT_LINE(SQLCODE);
+            ROLLBACK;
+    END;
+    ------PROCEDURE UPDATE------
+    
+    ------PROCEDURE DELETE------
+    
+END pkg_vote_x_person;
+    
+----------PACKAGE CATEGORY X PERSON----------
+CREATE OR REPLACE PACKAGE pkg_category_x_person IS
+    PROCEDURE insertFavoriteCategory(pIdNumber NUMBER, pCategoryCode NUMBER);
+END pkg_category_x_person;
+
+CREATE OR REPLACE PACKAGE BODY pkg_category_x_person AS
+    ------PROCEDURE INSERT------
+    PROCEDURE insertFavoriteCategory(pIdNumber NUMBER, pCategoryCode NUMBER) IS
+    BEGIN
+        INSERT INTO pc.category_x_person (id_number, category_code)
+        VALUES (pIdNumber, pCategoryCode);
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('Error inserting favorite category');
+            DBMS_OUTPUT.PUT_LINE(SQLERRM);
+            DBMS_OUTPUT.PUT_LINE(SQLCODE);
+            ROLLBACK;
+    END;
+    ------PROCEDURE UPDATE------
+    
+    ------PROCEDURE DELETE------
+    
+END pkg_category_x_person;
+    
+----------PACKAGE USER----------
+CREATE OR REPLACE PACKAGE pkg_user IS
+    PROCEDURE insertUser(pPassword VARCHAR2, pIdNumber NUMBER, pIdUserType NUMBER, pUserName VARCHAR2);
+END pkg_user;
+
+CREATE OR REPLACE PACKAGE BODY pkg_user AS
+    ------PROCEDURE INSERT------
+    PROCEDURE insertUser(pPassword VARCHAR2, pIdNumber NUMBER, pIdUserType NUMBER, pUserName VARCHAR2) IS
+    BEGIN
+        INSERT INTO pc.person_user(id_user,user_password,id_number,id_user_type,user_name)
+        VALUES(s_user_id.NEXTVAL,pPassword,pIdNumber,pIdUserType,pUserName);
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('Error inserting user');
+            DBMS_OUTPUT.PUT_LINE(SQLERRM);
+            DBMS_OUTPUT.PUT_LINE(SQLCODE);
+            ROLLBACK;
+    END;
+    ------PROCEDURE UPDATE------
+    
+    ------PROCEDURE DELETE------
+    
+END pkg_user;
+    
+----------PACKAGE NATIONALITY X PERSON----------
+CREATE OR REPLACE PACKAGE pkg_nationality_x_person IS
+    PROCEDURE insertNationalityXPerson(pIdNumber NUMBER, pIdNationality NUMBER);
+END pkg_nationality_x_person;
+
+CREATE OR REPLACE PACKAGE BODY pkg_nationality_x_person AS
+    ------PROCEDURE INSERT------
+    PROCEDURE insertNationalityXPerson(pIdNumber NUMBER, pIdNationality NUMBER) IS
+    BEGIN
+        INSERT INTO pc.nationality_x_person(id_number, id_nationality)
+        VALUES(pIdNumber,pIdNationality);
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('Error inserting nationality to person');
+            DBMS_OUTPUT.PUT_LINE(SQLERRM);
+            DBMS_OUTPUT.PUT_LINE(SQLCODE);
+            ROLLBACK;
+    END;
+    ------PROCEDURE UPDATE------
+    
+    ------PROCEDURE DELETE------
+    
+END pkg_nationality_x_person;
+
+----------PACKAGE PARAMETER----------
+CREATE OR REPLACE PACKAGE pkg_parameter IS
+    PROCEDURE insertParameter(pValue NUMBER, pName VARCHAR2);
+END pkg_parameter;
+
+CREATE OR REPLACE PACKAGE BODY pkg_parameter AS
+    ------PROCEDURE INSERT------
+    PROCEDURE insertParameter(pValue NUMBER, pName VARCHAR2) IS
+    BEGIN
+        INSERT INTO pc.parameter(id_parameter,parameter_value,parameter_name)
+        VALUES(s_parameter_id.NEXTVAL,pValue,pName);
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('Error inserting parameter');
+            DBMS_OUTPUT.PUT_LINE(SQLERRM);
+            DBMS_OUTPUT.PUT_LINE(SQLCODE);
+            ROLLBACK;
+    END;
+    ------PROCEDURE UPDATE------
+    
+    ------PROCEDURE DELETE------
+    
+END pkg_parameter;
