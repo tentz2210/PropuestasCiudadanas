7eff0671105c00e6a3389aa55ebd4f54b622635c
tentz2210 <tentz2210@hotmail.com>
2019-10-09 17:39:35 -0600

    added job script
    added job and schedule script for daily_top

---
+++ scripts_proyecto/jobs.sql
@@ -0,0 +1,20 @@
+--En pcadmin
+
+---------CREATE DAILY_TOP JOB---------
+BEGIN
+dbms_scheduler.create_schedule (
+	schedule_name => 'INTERVAL_DAILY_2300',
+	start_date => trunc(SYSDATE)+23/24,
+		--start today 23:00
+	repeat_interval => 'FREQ=DAILY;
+	BYDAY=MON,TUE,WED,THU,FRI,SAT,SUN;
+	BYHOUR=23;',
+	comments => 'Executes every day 23:00');
+    
+DBMS_SCHEDULER.CREATE_JOB(
+	job_name => 'DAILY_TOP_VOTED_REPORT',
+	job_type => 'PLSQL_BLOCK',
+	job_action => 'BEGIN
+	pkg_daily_top.generateDailyReport; END;',
+	schedule_name => 'INTERVAL_DAILY_2300' );
+END;
